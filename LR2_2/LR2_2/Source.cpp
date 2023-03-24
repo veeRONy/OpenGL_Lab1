88052d5d861ab4c2ff0a88ff1944adf75b3687a6
veeRONy <99815306+veeRONy@users.noreply.github.com>
2023-03-24 19:32:59 +0500

    Add files via upload

---
+++ LR2_2/LR2_2/Source.cpp
@@ -0,0 +1,211 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "pipeline.h"
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 768
+
+GLuint VBO;  // буфер вершин
+GLuint IBO;
+GLuint gWorldLocation; // указатель для доступа к всемирной матрице
+
+// используем вершинный шейдер
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+out vec4 Color;                                                                     \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+    Color = vec4(clamp(Position, 0.0, 1.0), 1.0);                                   \n\
+}";
+// используем пиксельный шейдер
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+in vec4 Color;                                                                      \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = Color;                                                              \n\
+}";
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    static float Scale = 0.0f;
+
+    // по сути скорость вращения
+    Scale += 0.1f;
+
+    Pipeline p;
+    p.Rotate(0.0f, Scale, 0.0f);
+    p.WorldPos(0.0f, 0.0f, 5.0f);
+    p.SetPerspectiveProj(30.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0f, 100.0f);
+
+    // функция для загрузки данных в uniform-переменные шейдера
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.GetTrans());
+
+    glEnableVertexAttribArray(0);
+    // привязываем массив вершин к VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Этот вызов говорит конвейеру как воспринимать данные внутри буфера
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    // функция для отрисовки
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+    glDisableVertexAttribArray(0);
+    // меняем фоновый буфер и буфер кадра местами
+    glutSwapBuffers();
+}
+
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+    glutIdleFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[4];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.5773f);
+    Vertices[1] = Vector3f(0.0f, -1.0f, -1.15475);
+    Vertices[2] = Vector3f(1.0f, -1.0f, 0.5773f);
+    Vertices[3] = Vector3f(0.0f, 1.0f, 0.0f);
+
+    // функция для генерации объектов
+    glGenBuffers(1, &VBO);
+    // привязываем массив вершин к VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // передаем данные в массив вершин
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void CreateIndexBuffer()
+{
+    unsigned int Indices[] = { 0, 3, 1,
+                               1, 3, 2,
+                               2, 3, 0,
+                               0, 2, 1 };
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    // создаем шейдер
+    GLuint ShaderObj = glCreateShader(ShaderType);
+    // проверка
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    // исходный код шейдера
+    const GLchar* p[1];
+    p[0] = pShaderText;
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText);
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    // компиляция шейдрера
+    glCompileShader(ShaderObj);
+    GLint success;
+    // проверка
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+    // присоединяем скомпилированный объект шейдера к объекту программы
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    // добавление шейдеров
+    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    // линковка программы шейдера
+    glLinkProgram(ShaderProgram);
+    // проверка
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    // еще проверка
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    // для использования отлинкованной программы шейдеров мы назначаем её для конвейера
+    glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 12");
+
+    InitializeGlutCallbacks();
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
+    CreateIndexBuffer();
+
+    CompileShaders();
+
+    glutMainLoop();
+
+    return 0;
+}
